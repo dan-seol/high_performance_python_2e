126abcb6675f3088c0e02d1ca1f90e33d3110521
dan-seol <yunheum.seol@mail.mcgill.ca>
2023-11-23 11:30:39 -0500

    pytorch-async

---
+++ 08_concurrency/crawler/tornado/crawler.py
@@ -0,0 +1,35 @@
+import asyncio
+import random
+import string
+
+from tornado.httpclient import AsyncHTTPClient
+
+AsyncHTTPClient.configure(
+    "tornado.curl_httpclient.CurlAsyncHTTPClient", max_clients=100  # <1>
+)
+
+
+def generate_urls(base_url, num_urls):
+    for i in range(num_urls):
+        yield base_url + "".join(random.sample(string.ascii_lowercase, 10))
+
+
+async def run_experiment(base_url, num_iter=1000):
+    http_client = AsyncHTTPClient()
+    urls = generate_urls(base_url, num_iter)
+    response_sum = 0
+    tasks = [http_client.fetch(url) for url in urls]  # <2>
+    for task in asyncio.as_completed(tasks):  # <3>
+        response = await task  # <4>
+        response_sum += len(response.body)
+    return response_sum
+
+
+if __name__ == "__main__":
+    import time
+    delay = 100
+    num_iter = 1000
+    start = time.time()
+    result = asyncio.run(run_experiment(f"http://127.0.0.1:8082/add?name=tornado&delay={delay}&", num_iter))  # <5>
+    end = time.time()
+    print(f"Result: {result}, Time: {end - start}")
